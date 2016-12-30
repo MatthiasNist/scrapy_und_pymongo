124b0d60133618e4521408d504e7d1ebb51d69a2
MatthiasNist <Matthias-Nist@hotmail.com>
2016-12-30 18:13:38 +0100

    mit Schleife und ohne Dot Notation

--- mininova2.py
+++ mininova2.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import numpy
 from scrapy.http.request import Request
 from scrapy.http import FormRequest
 from scrapy.spiders import CrawlSpider, Rule
@@ -11,18 +12,26 @@ from mininova2_spider.items import Mininova2SpiderItem
 class Mininova2Spider(scrapy.Spider):
     name = "mininova2_spiderl"
     allowed_domains = ["mininova.org"]
-    start_urls = ['http://www.mininova.org/tor/13364426']
+    #start_urls = ['http://www.mininova.org/tor/13364426']
+    
+    test =  numpy.arange(13364426, 13364428, 1)
+    iter = numpy.arange(0, len(test), 1)
+    start_urls = []
+
+    for i in iter:
+        start_urls.append("http://www.mininova.org/tor/" + str(test[i]) + "/")    
+    print(start_urls)
                                           
     def parse(self, response):
-        torrent = Mininova2SpiderItem()
+        torrent = Mininova2SpiderItem('')
         torrent["title"] = response.xpath("//div[@id='content']/h1/text()").extract()
         print(torrent["title"])
         torrent["size"] = response.xpath("//div[@id='specifications']/p[2]/text()[2]").extract()
         print(torrent["size"])
         torrent["language"] = response.xpath("//div[@id='specifications']/p[4]/text()[3]").extract()
         print(torrent["language"])
-        open_in_browser(response) # öffnet den Browser, mit der url nach der gecrawled wird
-        return(torrent)
+        #open_in_browser(response) # öffnet den Browser, mit der url nach der gecrawled wird
+        yield(torrent)
                                     
 
 
